083ec85c789e4d1fa3da520403412b849b58116e
mikebru <32722658+mikebru@users.noreply.github.com>
2021-04-06 17:19:20 -0500

    added in virtual production camera rig

--- Plugins/LensCalibrator/Source/LensCalibrator/Public/WrapperInterface.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) Polypulse LLC - All Rights Reserved
- * Written by Sean Connor <sean@polypulse.io>, April 2020 */
-#pragma once
-
-class WrapperInterface
-{
-public:
-	static void PollLog ();
-};
